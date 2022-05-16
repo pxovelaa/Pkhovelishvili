0ade4a152f0da7daa3c7438bf4eecdacf8a35c64
pxovelaa <gio.pxovelishvili@gmail.com>
2022-05-16 07:55:20 +0300

    Blog posts

---
+++ pxovela/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import "../src/App.css"
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    fetch(url)
+    .then(response => response.json())
+    .then(json => this.setState({ posts: json }))
+  }
+
+  render() {
+    const { posts } = this.state;
+    return (
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">Blog posts</h1>
+        </div>
+        <div className='blogs'>
+            {posts.map((post) => (
+              <div className="card" key={post.id}>
+                <div className="card-header">
+                  #{post.id} {post.title}
+                </div>
+                <div className="card-body">
+                  <p className="card-text">{post.body}</p>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
